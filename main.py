c0c5791dc81da6fe3d9500f08341a1881d19bba3
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-09 14:23:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -129,9 +129,10 @@ def check(lista):
                 
 
 #The main game loop, or the main game while loop is below this line.
+namea = input("What's your name?:  ")
+nameb = input("What's your name?:  ")
 while another == True:
     print("It is player one's turn.")
-    namea = input("What's your name?:  ")
     playera = int(input("Enter the row you want to put X in(1, 2, 3):  "))
     playera -=1
     print(lista[playera])
@@ -146,7 +147,6 @@ while another == True:
 
     printlist()
     print("It is player two's turn.")
-    nameb = input("What's your name?:  ")
     playerb = int(input("Enter the row you want to put O in(1, 2, 3):  "))
     playerb -=1
     print(lista[playerb])
