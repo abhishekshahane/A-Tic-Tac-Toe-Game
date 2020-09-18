ea45eb08a79cefb95e06dcee60c5eca717511bcf
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-09-18 13:44:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ lista =[
     ]
 
 another = True
+tieNotTrue = True
 def printlist():
     print("Tic-tac-toe Board:  ")
     print("\n")
     for row in lista:
         print(row)
     print("\n")
+def tie(lista):
+    for i in range(3):
+        for j in range(3):
+            if lista[i][j]!=None:
+                tieNotTrue = False
+            else:
+                tieNotTrue = True
+                return tieNotTrue
+    print("It is a tie!")
+    sleep(2)
+    exit()
 def check(lista):
     # The code below this is to BrainStorm every possible win combination.
     if (lista[0][0] == 'X' and lista[0][1] == 'X' and lista[0][2] == 'X'):
@@ -135,7 +147,7 @@ def check(lista):
 namea = input("What's your name?:  ")
 nameb = input("What's your name?:  ")
 printlist()
-while another == True:
+while another == True and tieNotTrue == True:
     print(f"It is {namea}'s turn.")
     playera = int(input("Enter the row you want to put X in(1, 2, 3):  "))
     playera -=1
@@ -149,8 +161,7 @@ while another == True:
     else:
         lista[playera][playerap] = 'X'
     check(lista)
-    
-
+    tie(lista)
     printlist()
     print(f"It is {nameb}'s turn.")
     playerb = int(input("Enter the row you want to put O in(1, 2, 3):  "))
@@ -166,4 +177,5 @@ while another == True:
         lista[playerb][playerbp] = 'O'
     # Checking every time after one move for any condition
     check(lista)
+    tie(lista)
     printlist()
